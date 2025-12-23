88f964ffb12b579dea0c58336437a6b6b87d88ca
Irfancpv99 <irfancpv99@gmail.com>
2025-12-23 19:07:09 +0100

    Add NoteView interface

---
+++ src/main/java/com/notemanager/view/NoteView.java
@@ -0,0 +1,23 @@
+package com.notemanager.view;
+
+import java.util.List;
+
+import com.notemanager.model.Category;
+import com.notemanager.model.Note;
+
+public interface NoteView {
+
+	void showAllNotes(List<Note> notes);
+
+	void showAllCategories(List<Category> categories);
+
+	void noteAdded(Note note);
+
+	void noteUpdated(Note note);
+
+	void noteDeleted(Note note);
+
+	void showError(String message);
+
+	void showErrorNoteNotFound(String message, Note note);
+}
