83d3e3810598646d39ec582d79142fa7b54741fa
Irfancpv99 <irfancpv99@gmail.com>
2025-12-27 20:22:21 +0100

    GREEN: Implement save update in CategoryMongoRepository

--- src/test/java/com/notemanager/it/repository/mongo/CategoryMongoRepositoryIT.java
+++ src/test/java/com/notemanager/it/repository/mongo/CategoryMongoRepositoryIT.java
@@ -12,6 +12,7 @@ import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Filters;
 import com.notemanager.model.Category;
 import com.notemanager.repository.mongo.CategoryMongoRepository;
 
