7e966a2aefa249031f98c9404bd856bce46f03d8
Irfancpv99 <irfancpv99@gmail.com>
2026-01-15 16:50:29 +0100

    Updated DatabaseInitilizer file

--- src/test/java/com/notemanager/unit/service/DatabaseInitializerTest.java
+++ src/test/java/com/notemanager/unit/service/DatabaseInitializerTest.java
@@ -5,6 +5,7 @@ import static java.util.Collections.*;
 import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.*;
 
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -33,6 +34,7 @@ class DatabaseInitializerTest {
 
 		verify(categoryRepository, times(3)).save(any(Category.class));
 	}
+
 	@Test
 	void testInitializeDefaultCategoriesWhenCategoriesAlreadyExist() {
 		Category existingCategory = new Category("EXISTING");
