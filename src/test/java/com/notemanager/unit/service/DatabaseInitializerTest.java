aec272aef02fc033059bf94d8b25b1f06946dbf1
Irfancpv99 <irfancpv99@gmail.com>
2026-01-15 16:14:49 +0100

    Refactor

---
+++ src/test/java/com/notemanager/unit/service/DatabaseInitializerTest.java
@@ -0,0 +1,37 @@
+package com.notemanager.unit.service;
+
+import static java.util.Arrays.*;
+import static java.util.Collections.*;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.notemanager.model.Category;
+import com.notemanager.repository.CategoryRepository;
+
+@ExtendWith(MockitoExtension.class)
+class DatabaseInitializerTest {
+
+	@Mock
+	private CategoryRepository categoryRepository;
+
+	@InjectMocks
+	private DatabaseInitializer databaseInitializer;
+
+	@Test
+	void testInitializeDefaultCategoriesWhenNoCategories() {
+		when(categoryRepository.findAll())
+			.thenReturn(emptyList());
+
+		databaseInitializer.initializeDefaultCategories();
+
+		verify(categoryRepository, times(3)).save(any(Category.class));
+	}
+
+	
+}
